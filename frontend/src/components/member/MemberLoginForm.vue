21789da83acf08d04bacbf58ff8480baca8755cf
Kimjoongyeon <jjjr69@naver.com>
2022-01-20 00:22:27 +0900

    로그인페이지 구성완료(기능테스트 해야함)

--- frontend/src/components/member/MemberLoginForm.vue
+++ frontend/src/components/member/MemberLoginForm.vue
@@ -1,14 +1,32 @@
 <template>
   <v-container>
-    <header>
-      
+    <header class="header">
+				<img src="@/assets/img/logo.png" class="logo" @click="Gomain" width="250px">
     </header>
-    <v-divider style="margin-bottom: 20px"></v-divider>
-    <div>
-      <a id="kakao_login" @click="kakaoLogin()">
-        <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" width="277"/>
+    
+    <div class="title">로그인 및 회원가입</div>
+    <div class="subTitle">로그인을 하여 다양한 혜택을 누리세요.</div>
+      <v-col sm="5">
+        <v-text-field v-model="email" :rules="emailRules" placeholder="이메일을 입력해주세요." max-width="400" required 
+          outlined ></v-text-field>
+      </v-col>
+			<v-col sm="5">
+        <v-text-field type="password" v-model="pw" :rules="pwRules" placeholder="비밀번호를 입력해주세요." required
+          outlined ></v-text-field>
+      </v-col>
+      <v-col sm="5">
+			  <v-btn width="400px" @click="onSubmit" class="white--text secondary" large block >로그인하기</v-btn>
+      </v-col>
+    <div class="float_line">
+      <div class="fl"></div><div class="fm">또는</div><div class="fr"></div>
+    </div>
+    <div style="padding:15px">
+      <div id="kakao_login">
+      <a @click="kakaoLogin()">
+        <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" width="277px"/>
       </a>
       <!--<button type="button" @click="kakaoLogout">카카오 로그아웃</button>-->
+      </div>
       <div>
         <div id="naverIdLogin"></div>
       </div>
@@ -17,7 +35,7 @@
     <div class="member_join">
       <span class="fs-15 fw-b">아직 가입하지 않으셨나요?</span><br>
       <spam>지금 바로 가입하여 더 많은 정보와 혜택을 누리세요.</spam>
-      <v-btn outlined style="margin-top: 20px">회원가입</v-btn>
+      <v-btn outlined style="margin-top: 20px" @click="Gomemberjoin">회원가입</v-btn>
     </div>
   </v-container>
 </template>
@@ -104,6 +122,12 @@ export default {
         console.log(res.data);
       });
     },
+    Gomain () {
+			this.$router.push({ name: 'Home' })
+		},
+    Gomemberjoin () {
+			this.$router.push({ name: 'MemberJoinPage' })
+		},
   },
 };
 </script>
@@ -111,16 +135,29 @@ export default {
 <style scoped>
   *{
 		font-family: 'Noto Sans KR', sans-serif;
-		margin: auto;
+		margin: 0 auto;
 	}
 
   #kakao_login{
-    width: 300px;
-    margin-left: 430px;
+    background-position: 0% 0%;
+    background-repeat: no-repeat;
+    text-align: center;
   }
 
   #naverIdLogin{
-    width: 300px;
+    width: 290px;
+    text-align: center;
+  }
+
+  .header{
+    text-align: center;
+  }
+
+  .logo{
+    text-align: center;
+    width: 250px;
+    display: inline;
+    cursor: pointer
   }
 
   .member_join{
@@ -131,5 +168,53 @@ export default {
     border: 2px solid grey;
   }
 
+  .title{
+    width: 400px;
+    height: 100px;
+    font-size: 20px;
+    font-weight: 700;
+    text-align: center;
+    border-bottom: 1px solid#E0E0E0;
+    padding: 50px 12px 25px 12px
+  }
+
+  .subTitle{
+    font-weight: 700;
+    width: 400px;
+    height: 68px;
+    text-align: center;
+    padding: 24px 0 24px 0;
+  }
+
+  .fl{
+    height: 7px;
+    width: 180px;
+    border-bottom: 1px solid  #E0E0E0;
+    float: left;
+    text-align: center;
+
+  }
+
+  .fm{
+    height: 17px;
+    width: 40px;
+    float: left;
+    text-align: center;
+  }
+
+  .fr {
+    height: 7px;
+    width: 180px;
+    border-bottom: 1px solid  #E0E0E0;
+    float: right;
+    text-align: center;
+  }
+
+  .float_line {
+    height: 45px;
+    width: 400px;
+    padding: 14px 0 14px 0;
+    
+  }
 
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <Header />
+    <div class="banner">
+      <swiper>
+				<swiper-slide><img class="swiper_bn" src="../assets/img/banner.jpg"></swiper-slide>
+			</swiper>
+				<div class=tabHouse>
+          <h1 class="main_text">솔직한 리뷰, 믿을 수 있는 평점</h1><p class="main_text">Yami!</p>
+        <fieldset class="main_search">
+          <input type="text" class="form_control" placeholder="지역, 식당 또는 음식" flat none>
+            <v-btn class="search-icon" height="50px" width="180px" color="#FF7100">
+              검색
+            </v-btn>
+        </fieldset>
+		  </div>
+    </div>
     <Footer />
   </div>
 </template>
@@ -8,6 +22,7 @@
 <script>
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
+
   export default {
     name: 'Home',
     components: {
@@ -15,3 +30,111 @@ import Footer from '../layout/Footer'
     },
   }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
+
+	* {
+		font-family: 'Noto Sans KR', sans-serif;
+		margin: 0 auto;
+	}
+
+.banner {
+  position: relative;
+}
+
+.search-icon {
+		/* Box */
+		height: 45px;
+		width: 180px;
+		margin: 150px 250px;
+		padding: 0px;
+    font-size: 21px;
+    line-height: 52px;
+
+
+		/* Positioning */
+		position: absolute;
+    top: 40px;
+    bottom: 0px;
+    right: -1px;
+    left: 261px;
+    float: none;
+    display: block;
+    color: white;
+
+    border-top-left-radius: 50px;
+		border-top-right-radius: 50px;
+		border-bottom-left-radius: 50px;
+		border-bottom-right-radius: 50px;
+	}
+
+.main_search {
+  width: 650px;
+  height: 50px;
+  border: 2px solid #FF7100;
+  margin: 100px 397px 0 0px;
+  background-color: white;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+
+}
+
+.form_control {
+  width: 400px;
+  height: 45px;
+  margin-left: 50px;
+}
+
+.main_text {
+		text-align: center;
+    line-height: 45px;
+		font-size: 30px;
+		font-weight: 450;
+		background: 0% 0%;
+		white-space: pre-wrap;
+		display: block;
+		/*margin: 0 129px 0 129px;*/
+    width: 720px;
+		color: white;
+	}
+
+  .swiper_bn {
+		height: 450px;
+		position: relative;
+    width: 100%;
+    display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		font-weight: bold;
+	}
+  .tabHouse::after {
+		display: block;
+		content: "";
+		position: absolute;
+		z-index: -1;
+		top: 80%;
+		left: 50%;
+		transform: translate( -50%, 0% );
+		min-width: 700px;
+		height: 300px;
+		opacity: 0.6;
+	}
+
+	.tabHouse {
+		align-content: center;
+		height: 174px;
+		width: 704px;
+		font-size: 16px;
+		display: block;
+		margin: 0 auto;
+		position: absolute;
+		top: 40%;
+		left: 50%;
+		transform: translate( -50%, -50% );
+	}
+
+</style>
